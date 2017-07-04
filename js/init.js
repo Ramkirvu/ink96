ebaeaaeee39df3386c4cce11ff7bc831d9edb575
Konstantin <Ramkirvu@gmail.com>
2017-07-04 22:52:02 +0300

    first

---
+++ js/init.js
@@ -0,0 +1,17 @@
+
+
+
+window.sr = ScrollReveal();
+sr.reveal('.row');
+sr.reveal('.col');
+
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
